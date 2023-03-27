fd4ca3c7ba80d47802c9188c111ec8247ae9dd5d
Vijay Pratap Singh <58662665+vijaypratap12@users.noreply.github.com>
2023-03-27 15:29:49 +0530

    Add project files.

---
+++ FirstMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using FirstMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace FirstMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult User()
+        {
+            HttpClient httpClient = new HttpClient();
+            UserModel users = new UserModel();
+            httpClient.BaseAddress = new Uri("https://localhost:7177");
+            int UserId = 1;
+            var response = httpClient.GetAsync($"/api/Employee/GetEmployeeByUserId/?UserId={UserId}");
+            response.Wait();
+            var result = response.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var display = result.Content.ReadAsAsync<UserModel>();
+                display.Wait();
+                users = display.Result;
+            }
+            return View(users);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FirstMVCProject/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstMVCProject.Models
+{
+    public class UserModel
+    {
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+            public string email { get; set; }
+        public DateTime updatedDate { get; set; }
+        public string isActive { get; set; }
+        public string isDelete { get; set; }
+    }
+}
