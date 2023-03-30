8537b40953eca589a9e05fa8fe66d1785ad377b5
admin <admin@DESKTOP-HUKQ8DH>
2023-03-30 05:09:20 -0700

    add complete mvc project

---
+++ SchoolManagementSystem MVC/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagementSystem_MVC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
